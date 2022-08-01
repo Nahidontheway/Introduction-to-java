a48fa110c1ce5c622c9d734d3fff9726658b3bce
Nahidontheway <100711410+Nahidontheway@users.noreply.github.com>
2022-08-01 21:08:35 +0600

    fundamental of javascript

---
+++ .vscode/fundamental/array.js
@@ -0,0 +1,3 @@
+var friendsAge = [ 15, 23, 12, 19, 25, 23 ];
+console.log(friendsAge);
+console.log(friendsAge.length);
---
+++ .vscode/fundamental/recap.js
@@ -0,0 +1,12 @@
+var isSingle = true;
+console.log(typeof isSingle);
+
+var number = 12;
+console.log(typeof number);
+
+var nameF = "fahrin";
+console.log(typeof nameF);
+
+var convertNum = 15;
+convertNum = (parseFloat(convertNum));
+console.log(convertNum);
