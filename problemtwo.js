35ddca9bb194982cf752eed38eb3974248a01601
Nahidontheway <100711410+Nahidontheway@users.noreply.github.com>
2022-08-01 12:00:16 +0600

    second problem solution

---
+++ problemtwo.js
@@ -0,0 +1,9 @@
+var math = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+var totalMarks = math+biology+chemistry+physics+bangla;
+var avgMarks = totalMarks/5;
+avgMarks = avgMarks.toFixed(2);
+console.log(avgMarks);
