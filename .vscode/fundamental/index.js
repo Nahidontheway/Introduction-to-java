f489d69b05a73cf0fbf03045fa874aae39f87b89
Nahidontheway <100711410+Nahidontheway@users.noreply.github.com>
2022-08-01 21:22:29 +0600

    array

---
+++ .vscode/fundamental/index.js
@@ -0,0 +1,12 @@
+//array declaration
+var numbers = [10, 20, 30, 40, 12, 16];
+console.log(numbers);
+//length find
+console.log(numbers.length);
+console.log(numbers[2]);
+//add  new number
+numbers[1]=56;
+console.log(numbers[1]);
+// find index number
+var positionIndex = numbers.indexOf(40);
+console.log(positionIndex);
