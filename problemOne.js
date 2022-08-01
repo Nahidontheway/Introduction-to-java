38b6e4d26ff95f52abafb06f347a904343e9bd58
Nahidontheway <100711410+Nahidontheway@users.noreply.github.com>
2022-08-01 11:44:39 +0600

    first problem solution

---
+++ problemOne.js
@@ -0,0 +1,6 @@
+var totalMoney = 1000;
+var orange = 300;
+var apple = 500;
+var purchaseAmount = orange+apple;
+var returnMoney = totalMoney - purchaseAmount;
+console.log(returnMoney);
--- variable.js
+++ variable.js
@@ -1,6 +1,18 @@
-var price = 40;
-console.log(price);
+// var price = 40;
+// console.log(price);
 
 // string
 
 var name ='person'
+
+var onionPrice = 20;
+var chiliPrice = 30;
+console.log(onionPrice);
+console.log(chiliPrice);
+var totalPrice = onionPrice + chiliPrice;
+console.log(totalPrice);
+
+var number1 = 17;
+var number2 = 5;
+var result = number1%number2;
+console.log(result);
